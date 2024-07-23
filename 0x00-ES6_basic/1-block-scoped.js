bf0edc2f4f50fcb5f506b0218d8aa088432ac666
hagarSamy <hs1800@fayoum.edu.eg>
2024-07-23 16:29:45 +0300

    hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task1 = true;
+    const task21 = false;
+    task = task21;
+    task2 = task1;
   }
 
   return [task, task2];
