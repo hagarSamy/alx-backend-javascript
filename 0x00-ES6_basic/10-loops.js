6d4571a5ebdf93168320a96006f94e2076d1cdf4
hagarSamy <hs1800@fayoum.edu.eg>
2024-07-24 01:50:24 +0300

    method in object in return

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArr = [];
-  for (const idx of array) {
-    const value = idx;
-    newArr.push(`${appendString}${value}`);
+    const newArr = [];
+    for (const idx of array) {
+      const value = idx;
+      newArr.push(`${appendString}${value}`);
+    }
+  
+    return newArr;
   }
-
-  return newArr;
-}
+  
