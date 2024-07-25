007c897aaca90b84101faa31defd1067038c612f
hagarSamy <hs1800@fayoum.edu.eg>
2024-07-25 04:41:48 +0300

    first promise

--- 0x01-ES6_promise/.eslintrc.js
+++ 0x01-ES6_promise/.eslintrc.js
@@ -1,46 +1,35 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
-import airbnb from 'eslint-config-airbnb-base';
-import jestPlugin from 'eslint-plugin-jest';
-
-const compat = new FlatCompat({
-  baseDirectory: new URL('.', import.meta.url).pathname,
-  resolvePluginsRelativeTo: new URL('.', import.meta.url).pathname,
-});
-
-export default [
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: {
-      jest: jestPlugin,
-    },
-    rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        {
-          selector: 'LabeledStatement',
-          message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
-        },
-        {
-          selector: 'WithStatement',
-          message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
-        },
-      ],
-      'import/no-mutable-exports': 'off', // Disable the problematic rule
-    },
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
   },
-  ...compat.extends('airbnb-base', 'plugin:jest/all'),
-  {
-    files: ['**/*.test.js'],
-    plugins: {
-      jest: jestPlugin,
-    },
-    rules: jestPlugin.configs.recommended.rules,
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-];
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
