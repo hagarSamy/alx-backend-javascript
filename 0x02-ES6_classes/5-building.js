bf0050d6eee27a587332f1b44ecf8ac11be1b88a
hagarSamy <hs1800@fayoum.edu.eg>
2024-07-26 09:43:29 +0300

    abstract class and method

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,10 +3,14 @@ export default class Building {
     this.sqft = sqft;
     if (new.target !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error("Class extending Building must override evacuationWarningMessage");
+        throw new Error('Class extending Building must override evacuationWarningMessage');
       }
     }
-}
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
 
   set sqft(newSqft) {
     this._sqft = newSqft;
