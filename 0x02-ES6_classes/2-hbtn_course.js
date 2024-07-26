132d4dd055d0a4f96c2d1582db8b903dfeebe2fe
hagarSamy <hs1800@fayoum.edu.eg>
2024-07-26 07:28:39 +0300

    getter and setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // getters for each atteibute
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // setters for each attr
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = newName.trim();
+    }
+  }
+
+  set length(newLen) {
+    if (typeof newLen !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = newLen;
+    }
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Students must be an array');
+    }
+    for (const student of newStudents) {
+      if (typeof student !== 'string') {
+        throw new TypeError('Each student must be a string');
+      }
+    }
+    this._students = newStudents;
+  }
+}
