25c3addf929844372e66dbe2a7dcc8d5f1e97196
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-03 13:53:19 +0300

    cleaning a set using substring method1

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 export default function cleanSet(set, startString) {
-  const myArr = [];
-  if (!startString) {
+  if (!startString || startString.length === 0) {
     return '';
   }
+  const myArr = [];
   for (const el of set) {
     if (el.startsWith(startString)) {
       // extracting a portion of each string, starting from a specific index
       myArr.push(el.substring(startString.length));
     }
   }
-  return myArr.join('-')
+  return myArr.join('-');
 }
