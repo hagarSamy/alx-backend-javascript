f73ad2d7ca6338f0cf783c4e955bde7f7c4c8eed
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-18 12:50:25 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,6 +2,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
-  process.stdout.write('This important software is now closing\n');
+//   process.stdout.write('This important software is now closing\n');
   process.exit();
+});
+process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
 });
