63951646d206dae85bee25a50ba3bd8e99fff5fe
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-19 04:07:32 +0300

    http header with endpoints

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,23 +4,15 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  
   if (req.url === '/') {
     res.write('Hello Holberton School!');
     res.end('');
   }
-  
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const result = countStudents(process.argv[2]) || 'database.csv';
-      countStudents(result)
-        .then((data) => {
-          res.end(`This is the list of our students\n${data}`);
-        })
-        .catch(error => {
-          res.write('Cannot load the database');
-        });
+      const result = countStudents(process.argv[2]);
+      res.write(result);
     } catch (error) {
       res.write('Cannot load the database');
     }
