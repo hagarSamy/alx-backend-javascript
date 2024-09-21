74a77a908320ba9bca6332c451afc6faba937583
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-21 04:14:11 +0300

    funny bugging

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -3,5 +3,5 @@ const Utils = require('./utils')
 
 module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const totalSum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${toString}`)
+  console.log(`The total is: ${totalSum}`)
 }
