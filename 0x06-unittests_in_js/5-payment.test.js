99aae637fc9be7d7c4195d086025f1a19ceed18a
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-21 04:27:04 +0300

    hooking

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,19 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+
+describe('hhoking sendPaymentRequestToApi', function(){
+  beforeEach(function() {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+  const expect = chai.expect;
+  it('should log "The total is: 120" when called with 100 and 20', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+  });
+  it('should log "The total is: 20" when called with 10 and 10', function() {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+  });
+});
