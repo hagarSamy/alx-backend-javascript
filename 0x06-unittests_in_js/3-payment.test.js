7adaae6a60bb9c01a06d7b8987edb4d422bc309c
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-21 03:40:10 +0300

    spy from sinon

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,12 +1,12 @@
 const sendPaymentRequestToApi = require('./3-payment.js')
-const sinon = require('sinon')
-import { expect } from 'chai';
+const { spy } = require('sinon');
+const { expect } = require('chai');
 const Utils = require('./utils.js')
 
 
 describe('spy sendPaymentRequestToApi' , function(){
   it('should call Utils.calculateNumber with SUM, 100, and 20', function(){
-    const spy = sinon.spy(Utils, 'calculateNumber');
+    const spy = spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
     expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
   });
