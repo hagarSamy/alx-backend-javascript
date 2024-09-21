2995091f82d22174dc45bc2973ec80bb2907ee2b
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-21 04:56:17 +0300

    async test

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function(){
+  it('should return a resolved promise with the correct data when success is true',
+    function(){
+      return new Promise ((done) => {
+        getPaymentTokenFromAPI(true)
+        .then(response => {
+        expect(response).to.deep.equal({ data:
+            'Successful response from the API' });
+        done();
+      });
+      })
+    });
+});
