5b7eb7f2f0c5ff19271f645d35e3fa813e3f8b1e
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-22 01:44:47 +0300

    basic express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+})
+
+
+app.listen(1245, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Listening on port 1245');
+  }
+});
+
+module.exports = app
